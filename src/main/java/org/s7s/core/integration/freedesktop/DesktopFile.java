090fb67eeb09d367b10d8615a9fb88c244d03e71
Tyler Cook <tcc@sandpolis.com>
2021-12-19 11:07:30 -0600

    Rename module

--- src/main/java/org/s7s/core/integration/freedesktop/DesktopFile.java
+++ src/main/java/org/s7s/core/integration/freedesktop/DesktopFile.java
@@ -1,20 +1,19 @@
 //============================================================================//
 //                                                                            //
-//                         Copyright © 2015 Sandpolis                         //
+//            Copyright © 2015 - 2022 Sandpolis Software Foundation           //
 //                                                                            //
 //  This source file is subject to the terms of the Mozilla Public License    //
-//  version 2. You may not use this file except in compliance with the MPL    //
-//  as published by the Mozilla Foundation.                                   //
+//  version 2. You may not use this file except in compliance with the MPLv2. //
 //                                                                            //
 //============================================================================//
-package com.sandpolis.core.integration.freedesktop;
+package org.s7s.core.integration.freedesktop;
 
 import java.nio.file.Path;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import com.sandpolis.core.foundation.S7SProcess;
+import org.s7s.core.foundation.S7SProcess;
 
 /**
  * Integration with .desktop files.
